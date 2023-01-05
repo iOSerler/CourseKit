22b5a05ca9a2e48a9d35a39c5ea5db034c54e40b
Nursultan Askarbekuly <nursultan@outlook.com>
2023-01-06 01:47:14 +0300

    clean up unnecessary code for LessonRow

--- CourseKit/CourseDetail/LessonRowView.swift
+++ CourseKit/CourseDetail/LessonRowView.swift
@@ -67,64 +67,10 @@ struct LessonRowView: View {
             
             Spacer()
         }
-        .onDidAppear {
+        .onAppear {
             
                 self.progress = lessonViewModel.getLessonProgress(userId: "nurios")
             
         }
     }
-}
-
-
-@available(iOS 15.0, *)
-extension View {
-  func onDidAppear(_ perform: @escaping (() -> Void)) -> some View {
-    self.modifier(ViewDidAppearModifier(callback: perform))
-  }
-}
-
-@available(iOS 15.0, *)
-struct ViewDidAppearModifier: ViewModifier {
-  let callback: () -> Void
-
-  func body(content: Content) -> some View {
-    content
-      .background(ViewDidAppearHandler(onDidAppear: callback))
-  }
-}
-
-@available(iOS 15.0, *)
-struct ViewDidAppearHandler: UIViewControllerRepresentable {
-  func makeCoordinator() -> ViewDidAppearHandler.Coordinator {
-    Coordinator(onDidAppear: onDidAppear)
-  }
-
-  let onDidAppear: () -> Void
-
-  func makeUIViewController(context: UIViewControllerRepresentableContext<ViewDidAppearHandler>) -> UIViewController {
-    context.coordinator
-  }
-
-  func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<ViewDidAppearHandler>) {
-  }
-
-  typealias UIViewControllerType = UIViewController
-
-  class Coordinator: UIViewController {
-    let onDidAppear: () -> Void
-
-    init(onDidAppear: @escaping () -> Void) {
-      self.onDidAppear = onDidAppear
-      super.init(nibName: nil, bundle: nil)
-    }
-
-    required init?(coder: NSCoder) {
-      fatalError("init(coder:) has not been implemented")
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-      super.viewDidAppear(animated)
-      onDidAppear()
-    }
-  }
 }
